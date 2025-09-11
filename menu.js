94ac8e05a12b41b656d4d78039ee7e6e4f2fc903
João Baptista Alves Ferreira Filho <bibliotecadotempo2025@gmail.com>
2025-09-11 18:26:21 -0300

    Atualizadas as páginas

--- menu.js
+++ menu.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuHTML = `
     <nav class="menu">
-      <div class="opcao"><a href="*">Volumes</a></div>
-      <div class="opcao"><a href="DGR.html" target="_blank">Décio Gonçalves Ribeiro</a></div>
-      <div class="opcao"><a href="JoseLanzellotti.html" target="_blank">José Lanzelotti</a></div>
-      <div class="opcao"><a href="*">Waldemar Seyssel - "Arrelia"</a></div>
+    <div class="opcao"><a href="Index.html" target="_blank">Início</a></div>
+    <div class="opcao"><a href="DecioGoncalves.html" target="_blank">Décio Gonçalves Ribeiro</a></div>
+    <div class="opcao"><a href="JoseLanzellotti.html" target="_blank">José Lanzelotti</a></div>
+    <div class="opcao"><a href="Arrelia.html" target="_blank">Waldemar Seyssel - "Arrelia"</a></div>
+    <div class="opcao"><a href="Volumes.html" target="_blank">Volumes</a></div>
+    <div class="opcao"><a href="Creditos.html" target="_blank">Créditos</a></div>
     </nav>
   `;
 
